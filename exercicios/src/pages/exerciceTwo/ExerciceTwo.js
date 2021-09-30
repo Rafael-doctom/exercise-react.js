41912d2c4a8b049312f24f9cb0972d460dd7e5d4
Rafael Pinto da Silva <dasilva.rafaelpt@gmail.com>
2021-09-30 17:57:57 -0300

    update exercise

---
+++ exercicios/src/pages/exerciceTwo/ExerciceTwo.js
@@ -0,0 +1,49 @@
+// list of Data
+// nothing style
+// calculate average
+
+
+import React from 'react';
+
+
+export default function ExerciceTwo() {
+
+    const Data = {
+        notebook: 'Samsung',
+        vehicle: 'Car',
+    };
+
+
+    const valueOne = 100;
+    const valueTwo = 200;
+    const valueThree = 300;
+
+    const Total = (valueOne + valueTwo + valueThree) / 3
+
+
+
+    return (
+        <>
+            <div>
+                <h5>ExerciceTwo </h5>
+                Modelo de notebook: {Data.notebook}
+                <br />
+                Tipo de Veículo: {Data.vehicle}
+            </div>
+            <div>
+                <h6>Média de valores</h6>
+                <p>
+                    Média 1: {valueOne}
+                    <br />
+                    Média 2: {valueTwo}
+                    <br />
+                    Média 3: {valueThree}
+                    <br />
+                </p>
+                <p>Total: {Total}</p>
+            </div>
+
+
+        </>
+    )
+}
