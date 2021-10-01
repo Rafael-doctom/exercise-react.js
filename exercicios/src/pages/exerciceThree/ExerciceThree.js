66cab636c732333613c07dacb70fbc7a30eea308
Rafael Pinto da Silva <dasilva.rafaelpt@gmail.com>
2021-10-01 09:57:33 -0300

    update exercise

---
+++ exercicios/src/pages/exerciceThree/ExerciceThree.js
@@ -0,0 +1,33 @@
+// console name
+//  data of purchase
+// total purchase
+
+import React from 'react';
+
+
+export default function ExerciceThree() {
+
+    const DataOfPurchase = {
+        name: 'Rafael',
+        age: 20,
+        situation: 'ativa',
+        purchaseOne: 100,
+        purchaseTwo: 300,
+        purchaseThree: 500,
+    }
+
+    return (
+        <>
+            <div>
+                <h6>ExerciceThree</h6>
+                Nome: {DataOfPurchase.name}
+                <br />
+                Idade: {DataOfPurchase.age}
+                <br />
+                Situação: {DataOfPurchase.situation}
+                <br />
+                Total gastos: {DataOfPurchase.purchaseOne + DataOfPurchase.purchaseTwo + DataOfPurchase.purchaseThree}
+            </div>
+        </>
+    )
+}
