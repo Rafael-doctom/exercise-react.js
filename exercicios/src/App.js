ae132302f68e64530cd511b51800dad03272dc92
Rafael Pinto da Silva <dasilva.rafaelpt@gmail.com>
2021-10-01 09:57:50 -0300

    update app

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -2,10 +2,15 @@ import React from 'react'
 import './App.js';
 
 import ExerciceOne from '../src/pages/exerciceOne/exerciceOne'
+import ExerciceTwo from '../src/pages/exerciceTwo/ExerciceTwo'
+import ExerciceThree from './pages/exerciceThree/ExerciceThree.js';
+
 function App() {
   return (
     <>
       <ExerciceOne />
+      <ExerciceTwo />
+      <ExerciceThree />
     </>
   );
 }
