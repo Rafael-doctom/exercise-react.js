c0ed1acd558abace2142b6b1de16f7888b73eac0
Rafael Pinto da Silva <dasilva.rafaelpt@gmail.com>
2021-10-01 11:35:04 -0300

    update exercise & clean code

--- exercicios/src/pages/exerciseFour/ExerciseFour.js
+++ exercicios/src/pages/exerciseFour/ExerciseFour.js
@@ -1,28 +1,40 @@
-// add List of Names
+// add List of Names and Books
 
 import React from 'react';
 
 
 export default function ExerciseFour() {
 
-    const names = ['Rafael', ' ', 'Luis', ' ', 'Kaio']
-    
+    const names = ['Rafael', ' ', 'Luis', ' ', 'Kaio'];
+
     const nameObject = {
         nomeOne: 'Rafael',
         nomeTwo: 'Luis',
         nomeThree: 'Kaio',
-    }
+    };
+
+
+    const books = [
+        { nameOfBook: 'Pai rico, pai pobre' },
+        { nameOfBook: 'Senhor dos aneis' },
+    ];
 
 
     return (
         <>
+            <h6>ExerciseFour</h6>
             <div>
-                <h6>ExerciseFour</h6>
                 {names}
-                <br/>
+                <br />
                 {nameObject.nomeOne}{' '}
                 {nameObject.nomeTwo}{' '}
                 {nameObject.nomeThree}{' '}
+            </div>
+
+            <div>
+                {
+                    books.map(book => (<li>{book.nameOfBook}</li>))
+                }
             </div>
         </>
     )
