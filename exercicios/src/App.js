19c53fd17666ebe0ff8fc22b75dbea4db555c647
Rafael Pinto da Silva <dasilva.rafaelpt@gmail.com>
2021-10-04 09:55:06 -0300

    update App

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,18 +1,32 @@
 import React from 'react'
 import './App.js';
 
-import ExerciseOne from '../src/pages/exerciseOne/exerciseOne'
-import ExerciseTwo from '../src/pages/exerciseTwo/ExerciseTwo'
+import ExerciseOne from '../src/pages/exerciseOne/exerciseOne';
+import ExerciseTwo from '../src/pages/exerciseTwo/ExerciseTwo';
 import ExerciseThree from './pages/exerciseThree/ExerciseThree.js';
 import ExerciseFour from './pages/exerciseFour/ExerciseFour.js';
+import ExerciseFive from './pages/exerciseFive/ExerciseFive.js';
+import ExerciseSix from './pages/exerciseSix/ExerciseSix';
+import ExerciseSeven from './pages/exerciseSeven/ExerciseSeven.js';
+import ExerciseEight from './pages/exerciseEight/ExerciseEight.js';
+import ExerciseNine from './pages/exerciseNine/ExerciseNine.js';
 
 function App() {
+
+
   return (
     <>
       <ExerciseOne />
       <ExerciseTwo />
       <ExerciseThree />
       <ExerciseFour />
+      <ExerciseFive />
+      <ExerciseSix />
+      <ExerciseSeven 
+        TextProps ='passando texto como propriedade...'
+      />
+      <ExerciseEight />
+      <ExerciseNine />
     </>
   );
 }
