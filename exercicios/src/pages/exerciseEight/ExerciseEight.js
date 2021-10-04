6fe3a311f2682b2712c66984e5118847d8fdec20
Rafael Pinto da Silva <dasilva.rafaelpt@gmail.com>
2021-10-04 09:52:18 -0300

    udpate exercise

---
+++ exercicios/src/pages/exerciseEight/ExerciseEight.js
@@ -0,0 +1,31 @@
+// object products list
+
+
+
+import React from 'react';
+
+
+export default function ExerciseEight() {
+
+    const Products = [
+        { product: 'notebook', value: '3k' },
+        { product: 'Ps4', value: '2k' },
+        { product: 'Pc Gamer', value: '5k' },
+    ]
+
+    return (
+        <>
+            <h6>Products</h6>
+            <ul>
+                {Products.map(item => (
+                    <>
+                        <li key={item.product}>
+                            {item.product}{' '}{item.value}
+                        </li>
+                    </>
+                ))}
+            </ul>
+        </>
+    )
+
+}
