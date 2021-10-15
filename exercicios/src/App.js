4cf1e51cc56610f18766750a1243bdf0ca7df218
Rafael Pinto da Silva <dasilva.rafaelpt@gmail.com>
2021-10-15 16:58:05 -0300

    -- update structure App

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './App.js';
 
+import UserContext from './UserContext'
+
 import ExerciseOne from '../src/pages/exerciseOne/exerciseOne';
 import ExerciseTwo from '../src/pages/exerciseTwo/ExerciseTwo';
 import ExerciseThree from './pages/exerciseThree/ExerciseThree.js';
@@ -13,33 +15,46 @@ import ExerciseNine from './pages/exerciseNine/ExerciseNine.js';
 import ExerciseTen from './pages/exerciseTen/ExerciseTen.js';
 import ExerciseEleven from './pages/exerciseEleven/ExerciseEleven.js';
 import ExerciseTwelve from './pages/exerciseTwelve/ExerciseTwelve.js';
-import ExerciseThirteen from './pages/exerciseThirteen/ExerciseThirteen.js';
+// import ExerciseThirteen from './pages/exerciseThirteen/ExerciseThirteen.js';
 import ExerciseFourteen from './pages/exerciseFourteen/ExerciseFourteen';
 import ExerciseFifteen from './pages/exerciseFifteen/ExerciseFifteen.js';
 import ExerciseSixteen from './pages/exerciseSixteen/ExerciseSixteen'
+import ExerciseSeventeen from './pages/exerciseSeventeen/ExerciseSeventeen.js';
+import ExerciseEighteen from './pages/exerciseEighteen/ExerciseEighteen';
+
+import ExerciseTwenty from './pages/exerciseTwenty/ExerciseTwenty.js';
+import ExerciseTwentyOne from './pages/exerciseTwentyOne/ExerciseTwentyOne.js';
+
 function App() {
 
 
   return (
     <>
-      <ExerciseOne />
-      <ExerciseTwo />
-      <ExerciseThree />
-      <ExerciseFour />
-      <ExerciseFive />
-      <ExerciseSix />
-      <ExerciseSeven
-        TextProps='passando texto como propriedade...'
-      />
-      <ExerciseEight />
-      <ExerciseNine />
-      <ExerciseTen />
-      <ExerciseEleven />
-      <ExerciseTwelve />
-      <ExerciseThirteen />
-      <ExerciseFourteen />
-      <ExerciseFifteen />
-      <ExerciseSixteen />
+      <UserContext.Provider value={{nome: 'Rafael'}}>
+        <ExerciseOne />
+        <ExerciseTwo />
+        <ExerciseThree />
+        <ExerciseFour />
+        <ExerciseFive />
+        <ExerciseSix />
+        <ExerciseSeven
+          TextProps='passando texto como propriedade...'
+        />
+        <ExerciseEight />
+        <ExerciseNine />
+        <ExerciseTen />
+        <ExerciseEleven />
+        <ExerciseTwelve />
+        {/* <ExerciseThirteen /> */}
+        <ExerciseFourteen />
+        <ExerciseFifteen />
+        <ExerciseSixteen />
+        <ExerciseSeventeen />
+        <ExerciseEighteen />
+
+        <ExerciseTwenty />
+        <ExerciseTwentyOne />
+      </UserContext.Provider>
     </>
   );
 }
