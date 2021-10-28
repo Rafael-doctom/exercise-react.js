a481329ea0b4f5eb75127158c6f3bfccf4d6cd3d
Rafael Pinto da Silva <dasilva.rafaelpt@gmail.com>
2021-10-28 06:58:48 -0300

    -- update App

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -3,27 +3,31 @@ import './App.js';
 
 import UserContext from './UserContext'
 
-import ExerciseOne from '../src/pages/exerciseOne/exerciseOne';
-import ExerciseTwo from '../src/pages/exerciseTwo/ExerciseTwo';
-import ExerciseThree from './pages/exerciseThree/ExerciseThree.js';
-import ExerciseFour from './pages/exerciseFour/ExerciseFour.js';
-import ExerciseFive from './pages/exerciseFive/ExerciseFive.js';
-import ExerciseSix from './pages/exerciseSix/ExerciseSix';
-import ExerciseSeven from './pages/exerciseSeven/ExerciseSeven.js';
-import ExerciseEight from './pages/exerciseEight/ExerciseEight.js';
-import ExerciseNine from './pages/exerciseNine/ExerciseNine.js';
-import ExerciseTen from './pages/exerciseTen/ExerciseTen.js';
-import ExerciseEleven from './pages/exerciseEleven/ExerciseEleven.js';
-import ExerciseTwelve from './pages/exerciseTwelve/ExerciseTwelve.js';
-// import ExerciseThirteen from './pages/exerciseThirteen/ExerciseThirteen.js';
-import ExerciseFourteen from './pages/exerciseFourteen/ExerciseFourteen';
-import ExerciseFifteen from './pages/exerciseFifteen/ExerciseFifteen.js';
-import ExerciseSixteen from './pages/exerciseSixteen/ExerciseSixteen'
-import ExerciseSeventeen from './pages/exerciseSeventeen/ExerciseSeventeen.js';
-import ExerciseEighteen from './pages/exerciseEighteen/ExerciseEighteen';
+import ExerciseOne from '../src/pages/01exerciseOne/exerciseOne';
+import ExerciseTwo from '../src/pages/02exerciseTwo/ExerciseTwo';
+import ExerciseThree from './pages/03exerciseThree/ExerciseThree.js';
+import ExerciseFour from './pages/04exerciseFour/ExerciseFour.js';
+import ExerciseFive from './pages/05exerciseFive/ExerciseFive.js';
+import ExerciseSix from './pages/06exerciseSix/ExerciseSix';
+import ExerciseSeven from './pages/07exerciseSeven/ExerciseSeven.js';
+import ExerciseEight from './pages/08exerciseEight/ExerciseEight.js';
+import ExerciseNine from './pages/09exerciseNine/ExerciseNine.js';
+import ExerciseTen from './pages/10exerciseTen/ExerciseTen.js';
+import ExerciseEleven from './pages/11exerciseEleven/ExerciseEleven.js';
+import ExerciseTwelve from './pages/12exerciseTwelve/ExerciseTwelve.js';
+// import ExerciseThirteen from './pages/13exerciseThirteen/ExerciseThirteen.js';
+import ExerciseFourteen from './pages/14exerciseFourteen/ExerciseFourteen';
+import ExerciseFifteen from './pages/15exerciseFifteen/ExerciseFifteen.js';
+import ExerciseSixteen from './pages/16exerciseSixteen/ExerciseSixteen'
+import ExerciseSeventeen from './pages/17exerciseSeventeen/ExerciseSeventeen.js';
+import ExerciseEighteen from './pages/18exerciseEighteen/ExerciseEighteen';
 
-import ExerciseTwenty from './pages/exerciseTwenty/ExerciseTwenty.js';
-import ExerciseTwentyOne from './pages/exerciseTwentyOne/ExerciseTwentyOne.js';
+import ExerciseTwenty from './pages/20exerciseTwenty/ExerciseTwenty.js';
+import ExerciseTwentyOne from './pages/21exerciseTwentyOne/ExerciseTwentyOne.js';
+import ExerciseTwentyTwo from './pages/22exerciseTwentyTwo/ExerciseTwentyTwo.js';
+
+
+
 
 function App() {
 
@@ -54,6 +58,7 @@ function App() {
 
         <ExerciseTwenty />
         <ExerciseTwentyOne />
+        <ExerciseTwentyTwo /> {/* useState & useEffect & Sum Input*/}
       </UserContext.Provider>
     </>
   );
